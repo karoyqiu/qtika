03161264ae79de7890ce0445f865b0556c18b5fc
Roy QIU <karoyqiu@gmail.com>
2016-08-21 16:41:55 +0800

    Almost finish the MimeTypes class.

--- qtika-core/mime/mimetypes.cpp
+++ qtika-core/mime/mimetypes.cpp
@@ -17,10 +17,17 @@
 #include "stable.h"
 #include "mimetypes.h"
 
+#include <QBuffer>
+#include <QMutex>
+#include <QUrl>
+
+#include <qiodevicetransactionrollback.h>
+
 #include "detect/textdetector.h"
 #include "detect/xmlrootextractor.h"
 #include "magic.h"
 #include "mediatyperegistry.h"
+#include "metadata/metadata.h"
 #include "patterns.h"
 
 
@@ -31,6 +38,15 @@ namespace mime {
 
 class MimeTypesData : public QSharedData
 {
+public:
+    /**
+     * Use the MimeType hint to try to clarify or specialise the current
+     *  possible types list.
+     * If the hint is a specialised form, use that instead
+     * If there are multiple possible types, use the hint to select one
+     */
+    QList<MimeType> applyHint(const QList<MimeType> &possibleTypes, const MimeType &hint) const;
+
 public:
     /**
      * Name of the {@link #rootMimeType root} type, application/octet-stream.
@@ -79,12 +95,39 @@ public:
 
     /** Sorted list of all registered rootXML */
     QList<MimeType> xmls;
+
+    //QMutex *mutex;
 };
 
 
 const QString MimeTypesData::OCTET_STREAM("application/octet-stream");
 const QString MimeTypesData::PLAIN_TEXT("text/plain");
 const QString MimeTypesData::XML("application/xml");
+
+
+QList<MimeType> MimeTypesData::applyHint(const QList<MimeType> &possibleTypes, const MimeType &hint) const
+{
+    QList<MimeType> result;
+
+    if (possibleTypes.isEmpty())
+    {
+        result.append(hint);
+        return result;
+    }
+
+    for (const MimeType &type : possibleTypes)
+    {
+        if (hint == type || registry.isSpecializationOf(hint.type(), type.type()))
+        {
+            // Use just this type
+            result.append(hint);
+            return result;
+        }
+    }
+
+    // Hint didn't help, sorry
+    return possibleTypes;
+}
 
 
 MimeTypes::MimeTypes()
@@ -124,7 +167,21 @@ MimeTypes::~MimeTypes()
 }
 
 
-QList<MimeType> MimeTypes::getMimeType(const QByteArray &d) const
+void MimeTypes::init()
+{
+    for (const MimeType &type : data->types.values())
+    {
+        data->magics.append(type.magics());
+
+        if (type.hasRootXML())
+        {
+            data->xmls.append(type);
+        }
+    }
+}
+
+
+QList<MimeType> MimeTypes::getMimeType(const QByteArray &d)
 {
     if (d.isNull())
     {
@@ -193,13 +250,98 @@ QList<MimeType> MimeTypes::getMimeType(const QByteArray &d) const
     // Finally, assume plain text if no control bytes are found
     try
     {
-        // UNDONE: TextDetector detector = new TextDetector(getMinLength());
-        //qtika::detect::TextDetector detector(min)
+        qtika::detect::TextDetector detector(minLength());
+        QBuffer buffer;
+        buffer.setData(d);
+        buffer.open(QIODevice::ReadOnly);
+        MimeType type = forName(detector.detect(&buffer, Metadata()).toString());
+        result.append(type);
+        return result;
     }
     catch (const std::exception &)
     {
         return data->rootMimeTypes;
     }
+}
+
+
+MimeType MimeTypes::getMimeType(const QString &name) const
+{
+    MimeType type = data->patterns.matches(name);
+
+    if (!type.isNull())
+    {
+        return type;
+    }
+
+    type = data->patterns.matches(name.toLower());
+
+    if (!type.isNull())
+    {
+        return type;
+    }
+
+    return data->rootMimeType;
+}
+
+
+MimeType MimeTypes::getMimeType(const QFileInfo &file) const
+{
+    // UNDONE: return forName(new Tika(this).detect(file));
+    throw std::exception("Not implemented.");
+}
+
+
+MimeType MimeTypes::getRegisteredMimeType(const QString &name) const
+{
+    MediaType type = MediaType::parse(name);
+
+    if (type.isNull())
+    {
+        // TODO: throw MimeTypeException
+        std::string msg("Invalid media type name: ");
+        msg += name.toStdString();
+        throw std::invalid_argument(msg);
+    }
+
+    MediaType normalized = data->registry.normalize(type);
+    MimeType candidate = data->types.value(normalized);
+
+    if (!candidate.isNull())
+    {
+        return candidate;
+    }
+
+    if (normalized.hasParameters())
+    {
+        return data->types.value(normalized.baseType());
+    }
+
+    return MediaType();
+}
+
+
+void MimeTypes::setSuperType(const MimeType &type, const detect::Detector::MediaType &parent)
+{
+    data->registry.addSuperType(type.type(), parent);
+}
+
+
+void MimeTypes::addAlias(const MimeType &type, const detect::Detector::MediaType &alias)
+{
+    data->registry.addAlias(type.type(), alias);
+}
+
+
+void MimeTypes::addPattern(const MimeType &type, const QString &pattern, bool isRegex)
+{
+    data->patterns.add(pattern, isRegex, type);
+}
+
+
+MediaTypeRegistry MimeTypes::mediaTypeRegistry() const
+{
+    return data->registry;
 }
 
 
@@ -219,6 +361,149 @@ void MimeTypes::add(const MimeType &type)
     {
         data->xmls.append(type);
     }
+}
+
+
+int MimeTypes::minLength()
+{
+    // This needs to be reasonably large to be able to correctly detect
+    // things like XML root elements after initial comment and DTDs
+    return 64 * 1024;
+}
+
+
+MimeType MimeTypes::forName(const QString &name)
+{
+    MediaType type = MediaType::parse(name);
+
+    if (type.isNull())
+    {
+        // TODO: throw MimeTypeException
+        std::string msg("Invalid media type name: ");
+        msg += name.toStdString();
+        throw std::runtime_error(msg);
+    }
+
+    MediaType normalized = data->registry.normalize(type);
+    MimeType mime = data->types.value(normalized);
+
+    if (mime.isNull())
+    {
+        static QMutex mutex;
+        QMutexLocker locker(&mutex);
+
+        // Double check it didn't already get added while
+        //  we were waiting for the lock
+        mime = data->types.value(normalized);
+
+        if (mime.isNull())
+        {
+            mime = normalized;
+            add(mime);
+            data->types.insert(type, mime);
+        }
+    }
+
+    return mime;
+}
+
+
+QByteArray MimeTypes::readMagicHeader(QIODevice *stream)
+{
+    if (stream == Q_NULLPTR)
+    {
+        throw std::invalid_argument("InputStream is missing.");
+    }
+
+    QByteArray bytes(minLength(), 0);
+    int totalRead = 0;
+    int lastRead = stream->read(bytes.data(), bytes.length());
+
+    while (lastRead > 0)
+    {
+        totalRead += lastRead;
+
+        if (totalRead == bytes.length())
+        {
+            return bytes;
+        }
+
+        lastRead = stream->read(bytes.data() + totalRead, bytes.length() - totalRead);
+    }
+
+    bytes.resize(totalRead);
+    return bytes;
+}
+
+
+MediaType MimeTypes::detect(QIODevice *input, const Metadata &meta) const
+{
+    QList<MimeType> possibleTypes;
+
+    // Get type based on magic prefix
+    if (input != Q_NULLPTR)
+    {
+        internal::QIODeviceTransactionRollback rollback(input);
+        QByteArray prefix = readMagicHeader(input);
+        possibleTypes = const_cast<MimeTypes *>(this)->getMimeType(prefix);
+    }
+
+    // Get type based on resourceName hint (if available)
+    QString resourceName = meta.get(Metadata::RESOURCE_NAME_KEY());
+
+    if (!resourceName.isEmpty())
+    {
+        QString name;
+
+        // Deal with a URI or a path name in as the resource name
+        QUrl url = QUrl::fromUserInput(resourceName);
+
+        if (url.isValid())
+        {
+            QString path = url.path();
+
+            if (!path.isEmpty())
+            {
+                int slash = path.lastIndexOf('/');
+
+                if (slash + 1 < path.length())
+                {
+                    name = path.mid(slash + 1);
+                }
+            }
+        }
+        else
+        {
+            name = resourceName;
+        }
+
+        if (!name.isEmpty())
+        {
+            MimeType hint = getMimeType(name);
+
+            // If we have some types based on mime magic, try to specialise
+            //  and/or select the type based on that
+            // Otherwise, use the type identified from the name
+            possibleTypes = data->applyHint(possibleTypes, hint);
+        }
+    }
+
+    // Get type based on metadata hint (if available)
+    QString typeName = meta.get(Metadata::CONTENT_TYPE());
+
+    if (!typeName.isEmpty())
+    {
+        MimeType hint = const_cast<MimeTypes *>(this)->forName(typeName);
+        possibleTypes = data->applyHint(possibleTypes, hint);
+    }
+
+    if (possibleTypes.isEmpty())
+    {
+        // Report that we don't know what it is
+        return MediaType::OCTET_STREAM();
+    }
+
+    return possibleTypes.first().type();
 }
 
 
--- qtika-core/mime/mimetypes.h
+++ qtika-core/mime/mimetypes.h
@@ -19,8 +19,11 @@
 #include "qtika-core-global.h"
 #include "detect/detector.h"
 #include "mimetype.h"
+#include "mediatyperegistry.h"
 
 #include <QSharedDataPointer>
+
+class QFileInfo;
 
 
 namespace qtika {
@@ -55,6 +58,12 @@ public:
     virtual ~MimeTypes();
 
     /**
+     * Called after all configured types have been loaded.
+     * Initializes the magics and xmls sets.
+     */
+    void init();
+
+    /**
      * Returns the MIME type that best matches the given first few bytes
      * of a document stream. Returns application/octet-stream if no better
      * match is found.
@@ -69,7 +78,84 @@ public:
      * @param data first few bytes of a document stream
      * @return matching MIME type
      */
-    QList<MimeType> getMimeType(const QByteArray &d) const;
+    QList<MimeType> getMimeType(const QByteArray &d);
+
+    /**
+     * Find the Mime Content Type of a document from its name.
+     * Returns application/octet-stream if no better match is found.
+     *
+     * @deprecated Use {@link Tika#detect(String)} instead
+     * @param name of the document to analyze.
+     * @return the Mime Content Type of the specified document name
+     */
+    Q_DECL_DEPRECATED MimeType getMimeType(const QString &name) const;
+
+    /**
+     * Find the Mime Content Type of a document stored in the given file.
+     * Returns application/octet-stream if no better match is found.
+     *
+     * @deprecated Use {@link Tika#detect(File)} instead
+     * @param file file to analyze
+     * @return the Mime Content Type of the specified document
+     * @throws MimeTypeException if the type can't be detected
+     * @throws IOException if the file can't be read
+     */
+    Q_DECL_DEPRECATED MimeType getMimeType(const QFileInfo &file) const;
+
+    /**
+     * Returns the registered, normalised media type with the given name (or alias).
+     *
+     * <p>Unlike {@link #forName(String)}, this function will <em>not<em> create a
+     * new MimeType and register it. Instead, <code>null</code> will be returned if
+     * there is no definition available for the given name.
+     *
+     * <p>Also, unlike {@link #forName(String)}, this function may return a
+     * mime type that has fewer parameters than were included in the supplied name.
+     * If the registered mime type has parameters (e.g.
+     * <code>application/dita+xml;format=map</code>), then those will be maintained.
+     * However, if the supplied name has paramenters that the <em>registered</em> mime
+     * type does not (e.g. <code>application/xml; charset=UTF-8</code> as a name,
+     * compared to just <code>application/xml</code> for the type in the registry),
+     * then those parameters will not be included in the returned type.
+     *
+     * @param name media type name (case-insensitive)
+     * @return the registered media type with the given name or alias, or null if not found
+     * @throws MimeTypeException if the given media type name is invalid
+     */
+    MimeType getRegisteredMimeType(const QString &name) const;
+
+    void setSuperType(const MimeType &type, const MediaType &parent);
+
+    /**
+     * Adds an alias for the given media type. This method should only
+     * be called from {@link MimeType#addAlias(String)}.
+     *
+     * @param type media type
+     * @param alias media type alias (normalized to lower case)
+     */
+    void addAlias(const MimeType &type, const MediaType &alias);
+
+    /**
+     * Adds a file name pattern for the given media type. The caller can specify
+     * whether the pattern being added <b>is</b> or <b>is not</b> a JDK standard
+     * regular expression via the <code>isRegex</code> parameter. If the value
+     * is set to true, then a JDK standard regex is assumed, otherwise the
+     * freedesktop glob type is assumed.
+     *
+     * @param type
+     *            media type
+     * @param pattern
+     *            file name pattern
+     * @param isRegex
+     *            set to true if JDK std regexs are desired, otherwise set to
+     *            false.
+     * @throws MimeTypeException
+     *             if the pattern conflicts with existing ones.
+     *
+     */
+    void addPattern(const MimeType &type, const QString &pattern, bool isRegex = false);
+
+    MediaTypeRegistry mediaTypeRegistry() const;
 
     /**
      * Add the specified mime-type in the repository.
@@ -78,6 +164,57 @@ public:
      *            is the mime-type to add.
      */
     void add(const MimeType &type);
+
+    /**
+     * Return the minimum length of data to provide to analyzing methods based
+     * on the document's content in order to check all the known MimeTypes.
+     *
+     * @return the minimum length of data to provide.
+     * @see #getMimeType(byte[])
+     * @see #getMimeType(String, byte[])
+     */
+    Q_DECL_CONSTEXPR static int minLength();
+
+    /**
+     * Returns the registered media type with the given name (or alias).
+     * The named media type is automatically registered (and returned) if
+     * it doesn't already exist.
+     *
+     * @param name media type name (case-insensitive)
+     * @return the registered media type with the given name or alias
+     * @throws MimeTypeException if the given media type name is invalid
+     */
+    MimeType forName(const QString &name);
+
+    /**
+     * Reads the first {@link #getMinLength()} bytes from the given stream.
+     * If the stream is shorter, then the entire content of the stream is
+     * returned.
+     * <p>
+     * The given stream is never {@link InputStream#close() closed},
+     * {@link InputStream#mark(int) marked}, or
+     * {@link InputStream#reset() reset} by this method.
+     *
+     * @param stream stream to be read
+     * @return first {@link #getMinLength()} (or fewer) bytes of the stream
+     * @throws IOException if the stream can not be read
+     */
+    static QByteArray readMagicHeader(QIODevice *stream);
+
+    /**
+     * Automatically detects the MIME type of a document based on magic
+     * markers in the stream prefix and any given metadata hints.
+     * <p>
+     * The given stream is expected to support marks, so that this method
+     * can reset the stream to the position it was in before this method
+     * was called.
+     *
+     * @param input document stream, or <code>null</code>
+     * @param metadata metadata hints
+     * @return MIME type of the document
+     * @throws IOException if the document stream could not be read
+     */
+    virtual MediaType detect(QIODevice *input, const Metadata &meta) const Q_DECL_OVERRIDE;
 
 private:
     QSharedDataPointer<MimeTypesData> data;
